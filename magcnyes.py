a900a0b368bb5b660db16c78588beca28ae7548e
Tzeng Yuxio <tzengyuxio@gmail.com>
2017-06-18 23:26:51 +0800

    Add MagCnyes crawler

--- magcnyes.py
+++ magcnyes.py
@@ -2,34 +2,31 @@
 """Crawler of Books.com
 """
 
+import calendar
 import json
-import logging
 import sqlite3
 import sys
 from datetime import datetime
 from random import randint
 from time import sleep
-from urllib.error import HTTPError
-from urllib.request import urlopen
+from urllib.request import Request, urljoin, urlopen
 
 from bs4 import BeautifulSoup
 
 PARSER = 'html.parser'
 
-MONTHTOPB = 'http://www.books.com.tw/web/sys_monthtopb/books/?year={0}&month={1}'
-PRODUCT = 'http://www.books.com.tw/products/{0}'
-SERIALTEXT = 'http://www.books.com.tw/web/sys_serialtext/?item={0}'
-SERIALTEXT_PAGE = SERIALTEXT + '&page={1}'
+NEWARTICLE = 'http://mag.cnyes.com/WebService/WebAjaxSvr.asmx/NewArticle'
 
-SQL_CREATE_BOOK_TOPS = '''
+SQL_CREATE_MAGCNYES_RANKINGS = '''
 CREATE TABLE IF NOT EXISTS
-    book_tops (year INTEGER, month INTEGER, top INTEGER, book_no TEXT, title TEXT, author TEXT,
-    PRIMARY KEY(year, month, top))
+    rankings (year INTEGER, month INTEGER, col_id INTEGER, top INTEGER,
+    col_name TEXT, art_id TEXT, title TEXT, full_title TEXT, mag_name TEXT, url TEXT,
+    PRIMARY KEY(year, month, col_id, top))
 '''
-SQL_CREATE_BOOKS = '''
+SQL_CREATE_MAGCNYES_ARTICLES = '''
 CREATE TABLE IF NOT EXISTS
-    books (book_no TEXT, title TEXT, author TEXT, page_count INTEGER, cont TEXT,
-    PRIMARY KEY(book_no))
+    articles (art_id TEXT, col_id INTEGER, col_name TEXT, title TEXT, full_title TEXT, mag_name TEXT, url TEXT, cont TEXT,
+    PRIMARY KEY(art_id))
 '''
 # num_char:   number of character in raw_text except space and new-line
 # num_hanzi:  number of hanzi in raw_text
@@ -39,17 +36,17 @@ CREATE TABLE IF NOT EXISTS
     corpus (src TEXT, idx TEXT, raw_text TEXT, stats TEXT, num_char INTEGER, num_hanzi INTEGER, num_unique INTEGER,
     PRIMARY KEY(src, idx))
 '''
-SQL_INSERT_BOOK_TOPS = '''
-INSERT OR IGNORE INTO book_tops VALUES (?, ?, ?, ?, ?, ?)
+SQL_INSERT_MAGCNYES_RANKINGS = '''
+INSERT OR IGNORE INTO rankings VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
 '''
-SQL_INSERT_BOOKS = '''
-INSERT OR IGNORE INTO books VALUES (?, ?, ?, ?, ?)
+SQL_INSERT_MAGCNYES_ARTICLES = '''
+INSERT OR IGNORE INTO articles VALUES (?, ?, ?, ?, ?, ?, ?, ?)
 '''
 SQL_INSERT_CORPUS = '''
 INSERT OR IGNORE INTO corpus VALUES (?, ?, ?, ?, ?, ?, ?)
 '''
-SQL_EXISTS_BOOKS = '''
-SELECT 1 FROM books WHERE book_no=?
+SQL_EXISTS_ARTICLE = '''
+SELECT 1 FROM articles WHERE art_id=?
 '''
 SQL_SELECT_BOOKS = '''
 SELECT * FROM books WHERE page_count > 0
@@ -82,15 +79,24 @@ def is_unihan_ext(char):
             '\U0002a700' <= char <= '\U0002ceaf')  # CJK Unified Ideographs Extension C,D,E
 
 
+def month_range(year, month):
+    """month_range
+    """
+    first, last = calendar.monthrange(year, month)
+    first_date = '{0:04}-{1:02}-{2:02}'.format(year, month, first)
+    last_date = '{0:04}-{1:02}-{2:02}'.format(year, month, last)
+    return first_date, last_date
+
+
 class SqliteWriter():
     """SQLite Writer
     """
 
     def __init__(self):
-        self.conn = sqlite3.connect('books.db')
+        self.conn = sqlite3.connect('magcnyes.db')
         cur = self.conn.cursor()
-        cur.execute(SQL_CREATE_BOOK_TOPS)
-        cur.execute(SQL_CREATE_BOOKS)
+        cur.execute(SQL_CREATE_MAGCNYES_RANKINGS)
+        cur.execute(SQL_CREATE_MAGCNYES_ARTICLES)
         self.conn.commit()
         cur.close()
 
@@ -100,29 +106,27 @@ class SqliteWriter():
         self.corpus.commit()
         cur.close()
 
-    def write_top(self, year, month, top_no, book_no, title, author):
+    def write_ranking(self, values):
         """write_ranking
         """
         cur = self.conn.cursor()
-        cur.execute(SQL_INSERT_BOOK_TOPS, (year, month,
-                                           top_no, book_no, title, author))
+        cur.execute(SQL_INSERT_MAGCNYES_RANKINGS, values)
         self.conn.commit()
         cur.close()
 
-    def write_book(self, book_no, title, author, page_count, cont):
-        """write_book
+    def write_article(self, values):
+        """write_article
         """
         cur = self.conn.cursor()
-        cur.execute(SQL_INSERT_BOOKS,
-                    (book_no, title, author, page_count, cont))
+        cur.execute(SQL_INSERT_MAGCNYES_ARTICLES, values)
         self.conn.commit()
         cur.close()
 
-    def contains_book(self, book_no):
+    def contains_article(self, art_id):
         """contains_book
         """
         cur = self.conn.cursor()
-        cur.execute(SQL_EXISTS_BOOKS, [book_no])
+        cur.execute(SQL_EXISTS_ARTICLE, [art_id])
         self.conn.commit()
         result = True if cur.fetchone() is not None else False
         cur.close()
@@ -149,32 +153,32 @@ class SqliteWriter():
             num_hanzi = sum(char_freq_table.values())
             num_unique = len(char_freq_table)
             stats = json.dumps(char_freq_table, sort_keys=True)
-            cur_ins = self.corpus.cursor()
+            cur_ins = self.conn.cursor()
             cur_ins.execute(SQL_INSERT_CORPUS, (src, idx, raw_text,
                                                 stats, num_char, num_hanzi, num_unique))
             print('{0} [INFO] Calc book[{1}] ... num(char/hanzi/unique) = {2}/{3}/{4}'.format(
                 datetime_iso(), row[0], num_char, num_hanzi, num_unique))
-            self.corpus.commit()
+            self.conn.commit()
             cur_ins.close()
         cur.close()
 
 
-class Books():
-    """Crawler of Books.com
+class MagCnyes():
+    """Crawler of MagCnyes
     """
 
     def __init__(self, writer):
         self.writer = writer
         self.urlopen_count = 0
-        logging.basicConfig(
-            format='%(asctime)s %(levelname)s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+        self.columns = {1: u'時尚', 2: u'生活', 7: u'醫美', 8: u'旅遊',
+                        9: u'藝文', 10: u'設計', 3: u'商業', 5: u'理財', 6: u'科技'}
 
     def sleep(self):
         """sleep
         """
         self.urlopen_count += 1
         if self.urlopen_count % 100 == 0:
-            for _ in range(0, randint(24, 96), 2):
+            for _ in range(0, randint(24, 36), 2):
                 print('=', end='', flush=True)
                 sleep(2)
         elif self.urlopen_count % 10 == 0:
@@ -182,77 +186,75 @@ class Books():
                 print('-', end='', flush=True)
                 sleep(1)
         else:
-            sleep(randint(3, 5))
+            sleep(randint(1, 3))
 
-    def test_book(self, book_no):
-        """test_book
+    def fetch_article(self, art_id, col_id, title, full_title, mag_name, url_first):
+        """fetch_article
         """
-        self.sleep()
-        url = SERIALTEXT.format(book_no)
-        try:
-            soup = BeautifulSoup(urlopen(url), PARSER)
-            page_count = int(soup.find_all(
-                'div', {'class': 'page'})[-1].span.text)
-            soup.decompose()
-            return page_count
-        except HTTPError:
-            return 0
-
-    def fetch_book(self, book_no, title, author):
-        """fetch_book
-        """
-        print('{0} [INFO]   Fetching Book[{1}] {2}'.format(
-            datetime_iso(), book_no, title), end='', flush=True)
-        if self.writer.contains_book(book_no):
+        print('{0} [INFO]   Fetching Article: {1}'.format(
+            datetime_iso(), title), end='', flush=True)
+        if self.writer.contains_article(art_id):
             print(' -> contained and skip')
             return
-        page_count = self.test_book(book_no)
-        if page_count <= 0:
-            self.writer.write_book(book_no, title, author, page_count, '')
-            print(' -> no preview')
-            return
-        # text = '{0}\n\n{1}\n'.format(title, author)
+
         text = ''
-        for i in range(1, page_count + 1):
+        url_base = urljoin(NEWARTICLE, url_first)
+        url_page = url_first
+        while url_page is not None:
             print('.', end='', flush=True)
             self.sleep()
-            url = SERIALTEXT_PAGE.format(book_no, i)
+            url = urljoin(url_base, url_page)
+            # print(url)
             soup = BeautifulSoup(urlopen(url), PARSER)
-            cont = soup.find_all('div', {'class': 'cont'})[-1].text
-            text += cont
+            text += soup.find_all('div', {'class': 'content'})[0].text
+            bnext_btns = soup.find_all('a', {'class': 'bnext'})
+            url_page = None if len(
+                bnext_btns) == 0 else bnext_btns[0].get('href')
             soup.decompose()
-        self.writer.write_book(book_no, title, author, page_count, text)
+
+        # write
+        self.writer.write_article(
+            [art_id, col_id, self.columns[col_id], title, full_title, mag_name, url_first, text])
         print('saved')
         return
 
-    def fetch_month(self, year, month):
+    def fetch_month(self, year, month, col_id):
         """fetch_mont
         """
-        print('{0} [INFO] Processing Top100 of {1}-{2:02}'.format(
-            datetime_iso(), year, month))
-        self.sleep()
-        url = MONTHTOPB.format(year, month)
-        soup = BeautifulSoup(urlopen(url), PARSER)
-        for div in soup.find_all('div', {'class': 'type02_bd-a'}):
-            top_no = div.parent.find_all('strong', {'class': 'no'})[0].text
-            title = div.h4.text
-            author = div.ul.li.text[3:]
-            href = div.h4.a.get('href')
-            book_no = href.rsplit('/', 1)[-1].split('?')[0]
-            self.writer.write_top(year, month, top_no, book_no, title, author)
-            self.fetch_book(book_no, title, author)
-        soup.decompose()
+        print('{0} [INFO] Processing Top100 of {1}-{2:02} at {3}'.format(
+            datetime_iso(), year, month, self.columns[col_id]))
+        page_size = 100
+        start_date, end_date = month_range(year, month)
+        req_body_json = '{{"Start":"{2}","End":"{3}","ColumnID":{0},"PageSize":{1},"PageIndex":1}}'
+        req_data = req_body_json.format(
+            col_id, page_size, start_date, end_date)
+        req = Request(url=NEWARTICLE, data=req_data.encode(encoding='utf_8'))
+        req.add_header('Content-Type', 'application/json')
+        with urlopen(req) as resp:
+            data = json.loads(resp.read().decode('utf-8'))['d']
+            for idx, art in enumerate(data['List']):
+                art_id = art['ArticleID']
+                full_title = art['FullTitle']
+                mag_name = art['MagName']
+                title = art['Title']
+                url = art['Url']
+                self.writer.write_ranking(
+                    [year, month, col_id, idx + 1, self.columns[col_id],
+                     art_id, title, full_title, mag_name, url])
+                self.fetch_article(art_id, col_id, title,
+                                   full_title, mag_name, url)
 
     def fetch_all(self):
         """fetch_all
         """
-        start = (2013, 7)
-        end = (2017, 6)
+        start = (2017, 4)
+        end = (2017, 5)
         for year in range(start[0], end[0] + 1):
             start_month = start[1] if year == start[0] else 1
             end_month = end[1] if year == end[0] else 13
             for month in range(start_month, end_month):
-                self.fetch_month(year, month)
+                for col_id in self.columns:
+                    self.fetch_month(year, month, col_id)
 
     def calc_all(self):
         """calc_all
@@ -278,13 +280,13 @@ if __name__ == '__main__':
         sys.exit(0)
     elif sys.argv[1] == 'fetch':
         WRITER = SqliteWriter()
-        BOOK = Books(WRITER)
-        BOOK.fetch_all()
-        # BOOK.fetch_month(2017, 4)
+        MAG = MagCnyes(WRITER)
+        # MAG.fetch_all()
+        MAG.fetch_month(2017, 4, 7)
         # print(BOOK.test_book('0010723234'))
         # print(BOOK.fetch_book('0010723234', '為了活下去：脫北女孩朴研美', '朴研美', 5))
         # BOOK.fetch_book('0010723234', '為了活下去：脫北女孩朴研美', '朴研美')
     elif sys.argv[1] == 'calc':
         WRITER = SqliteWriter()
-        BOOK = Books(WRITER)
-        BOOK.calc_all()
+        MAG = MagCnyes(WRITER)
+        MAG.calc_all()
