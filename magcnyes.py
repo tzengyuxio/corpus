1af0f651ea9ae48aac2acd9158fd74af191b7e8d
Tzeng Yuxio <tzengyuxio@gmail.com>
2017-06-24 17:48:45 +0800

    Refactor magcnyes.py

--- magcnyes.py
+++ magcnyes.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """Crawler of Mag Cnyes.com
 """
-
 import json
+import logging
 import sqlite3
 import sys
 from random import randint
@@ -11,11 +11,34 @@ from urllib.request import Request, urljoin, urlopen
 
 from bs4 import BeautifulSoup
 
-from utils import datetime_iso, is_unihan, month_range
+from utils import PARSER, datetime_iso, is_unihan, month_range
 
-PARSER = 'html.parser'
+SQL_CREATE_TABLE_ARTICLES = '''
+CREATE TABLE IF NOT EXISTS articles (
+    art_id TEXT, col_id INTEGER, col_name TEXT, publisher TEXT, pub_date TEXT, title TEXT, full_title TEXT, article TEXT,
+    PRIMARY KEY(art_id)
+)
+'''
+SQL_CREATE_TABLE_RANKINGS = '''
+CREATE TABLE IF NOT EXISTS rankings (
+    year INTEGER, month INTEGER, col_id INTEGER, ranking INTEGER,
+    col_name TEXT, art_id TEXT, title TEXT, full_title TEXT, mag_name TEXT, url TEXT,
+    PRIMARY KEY(year, month, col_id, ranking)
+)
+'''
+SQL_CONTAIN_ARTICLE = '''
+SELECT 1 FROM articles WHERE art_id=?
+'''
+SQL_INSERT_ARTICLE = '''
+INSERT OR IGNORE INTO articles
+    (art_id, col_id, col_name, publisher, pub_date, title, full_title, article) VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+'''
+SQL_INSERT_RANKING = '''
+INSERT OR IGNORE INTO rankings
+    (year, month, col_id, ranking, col_name, art_id, title, full_title, mag_name, url) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+'''
 
-NEWARTICLE = 'http://mag.cnyes.com/WebService/WebAjaxSvr.asmx/NewArticle'
+URL_NEWARTICLE = 'http://mag.cnyes.com/WebService/WebAjaxSvr.asmx/NewArticle'
 
 SQL_CREATE_MAGCNYES_RANKINGS = '''
 CREATE TABLE IF NOT EXISTS
@@ -39,14 +62,8 @@ CREATE TABLE IF NOT EXISTS
 SQL_INSERT_MAGCNYES_RANKINGS = '''
 INSERT OR IGNORE INTO rankings VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
 '''
-SQL_INSERT_MAGCNYES_ARTICLES = '''
-INSERT OR IGNORE INTO articles VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
-'''
 SQL_INSERT_CORPUS = '''
 INSERT OR IGNORE INTO corpus VALUES (?, ?, ?, ?, ?, ?, ?)
-'''
-SQL_EXISTS_ARTICLE = '''
-SELECT 1 FROM articles WHERE art_id=?
 '''
 SQL_SELECT_ARTICLES = '''
 SELECT art_id, full_title, cont FROM articles WHERE substr(published, 0, 5)="2016"
@@ -59,17 +76,11 @@ class SqliteWriter():
 
     def __init__(self):
         self.conn = sqlite3.connect('magcnyes.db')
-        cur = self.conn.cursor()
-        cur.execute(SQL_CREATE_MAGCNYES_RANKINGS)
-        cur.execute(SQL_CREATE_MAGCNYES_ARTICLES)
-        self.conn.commit()
-        cur.close()
-
-        self.corpus = sqlite3.connect('corpus.db')
-        cur = self.corpus.cursor()
-        cur.execute(SQL_CREATE_CORPUS)
-        self.corpus.commit()
-        cur.close()
+        # cur = self.conn.cursor()
+        # cur.execute(SQL_CREATE_MAGCNYES_RANKINGS)
+        # cur.execute(SQL_CREATE_MAGCNYES_ARTICLES)
+        # self.conn.commit()
+        # cur.close()
 
     def write_ranking(self, values):
         """write_ranking
@@ -78,24 +89,6 @@ class SqliteWriter():
         cur.execute(SQL_INSERT_MAGCNYES_RANKINGS, values)
         self.conn.commit()
         cur.close()
-
-    def write_article(self, values):
-        """write_article
-        """
-        cur = self.conn.cursor()
-        cur.execute(SQL_INSERT_MAGCNYES_ARTICLES, values)
-        self.conn.commit()
-        cur.close()
-
-    def contains_article(self, art_id):
-        """contains_book
-        """
-        cur = self.conn.cursor()
-        cur.execute(SQL_EXISTS_ARTICLE, [art_id])
-        self.conn.commit()
-        result = True if cur.fetchone() is not None else False
-        cur.close()
-        return result
 
     def select_articles(self):
         """select_book
@@ -117,32 +110,19 @@ class SqliteWriter():
                                v in char_freq_table.items() if is_unihan(k)}
             num_hanzi = sum(char_freq_table.values())
             num_unique = len(char_freq_table)
-            stats = json.dumps(char_freq_table, ensure_ascii=False, sort_keys=True).encode('utf-8')
-            cur_ins = self.corpus.cursor()
-            cur_ins.execute(SQL_INSERT_CORPUS, (src, idx, raw_text,
-                                                stats, num_char, num_hanzi, num_unique))
+            stats = json.dumps(
+                char_freq_table, ensure_ascii=False, sort_keys=True).encode('utf-8')
+            # cur_ins = self.corpus.cursor()
+            # cur_ins.execute(SQL_INSERT_CORPUS, (src, idx, raw_text,
+            # stats, num_char, num_hanzi, num_unique))
             print('{0} [INFO] Calc article[{1}] ... num(char/hanzi/unique) = {2}/{3}/{4}'.format(
                 datetime_iso(), row[0], num_char, num_hanzi, num_unique))
-            cur_ins.close()
+            # cur_ins.close()
         cur.close()
-        self.corpus.commit()
-
-    def update_published(self):
-        """update_published
-        """
-        cur = self.conn.cursor()
-        for row in cur.execute('SELECT art_id, url FROM articles'):
-            art_id = row[0]
-            url = row[1]
-            published = '{0}-{1}-{2}'.format(url[9:13], url[13:15], url[15:17])
-            cur_up = self.conn.cursor()
-            cur_up.execute('UPDATE articles SET published=? WHERE art_id=?', (published, art_id))
-            self.conn.commit()
-            cur_up.close()
-        cur.close()
+        # self.corpus.commit()
 
 
-class MagCnyes():
+class MagCnyesCrawler():
     """Crawler of MagCnyes
     """
 
@@ -151,114 +131,183 @@ class MagCnyes():
         self.urlopen_count = 0
         self.columns = {1: u'時尚', 2: u'生活', 7: u'醫美', 8: u'旅遊',
                         9: u'藝文', 10: u'設計', 3: u'商業', 5: u'理財', 6: u'科技'}
+        self.init_db()
+        self.init_logger()
+        self.logger.info('---- [MagCnyesCrawler] ---------------------------')
 
-    def sleep(self):
+    def init_db(self):
+        """init db
+        """
+        self.conn = sqlite3.connect('source-magcnyes.db')
+        cur = self.conn.cursor()
+        cur.execute(SQL_CREATE_TABLE_ARTICLES)
+        cur.execute(SQL_CREATE_TABLE_RANKINGS)
+        self.conn.commit()
+        cur.close()
+
+    def init_logger(self):
+        """init logger
+        """
+        self.logger = logging.getLogger('magcnyes')
+        self.logger.setLevel(logging.DEBUG)
+        formatter = logging.Formatter(
+            '%(asctime)s %(levelname)-7s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+
+        ifh = logging.FileHandler('crawler-magcnyes.log', encoding='utf-8')
+        ifh.setFormatter(formatter)
+        ifh.setLevel(logging.INFO)
+        wfh = logging.FileHandler(
+            'crawler-magcnyes-warn.log', encoding='utf-8')
+        wfh.setFormatter(formatter)
+        wfh.setLevel(logging.WARN)
+        dsh = logging.StreamHandler()
+        dsh.setFormatter(formatter)
+        dsh.setLevel(logging.DEBUG)
+
+        self.logger.addHandler(ifh)
+        self.logger.addHandler(wfh)
+        self.logger.addHandler(dsh)
+
+    def sleep(self, sec=None):
         """sleep
         """
         self.urlopen_count += 1
+        if sec is not None:
+            sleep(sec)
+            return
         if self.urlopen_count % 100 == 0:
-            for _ in range(0, randint(30, 90), 2):
+            seconds = randint(30, 90)
+            print('(count: {0}, sleep {1} seconds) ['.format(
+                self.urlopen_count, seconds), end='')
+            for _ in range(0, seconds, 2):
                 print('=', end='', flush=True)
                 sleep(2)
+            print(']')
         elif self.urlopen_count % 10 == 0:
-            for _ in range(randint(5, 9)):
+            seconds = randint(5, 9)
+            print('(count: {0}, sleep {1} seconds) ['.format(
+                self.urlopen_count, seconds), end='')
+            for _ in range(seconds):
                 print('-', end='', flush=True)
                 sleep(1)
+            print(']')
         else:
             sleep(randint(1, 3))
 
-    def fetch_article(self, art_id, col_id, title, full_title, mag_name, url_first):
-        """fetch_article
+    def contain_article(self, art_id):
+        """check if contains article
         """
-        print('{0} [INFO]   Fetching Article: {1}'.format(
-            datetime_iso(), title), end='', flush=True)
-        if self.writer.contains_article(art_id):
-            print(' -> contained and skip')
+        cur = self.conn.cursor()
+        cur.execute(SQL_CONTAIN_ARTICLE, [art_id])
+        result = True if cur.fetchone() is not None else False
+        cur.close()
+        return result
+
+    def insert_article(self, article_values):
+        """insert article
+        """
+        cur = self.conn.cursor()
+        cur.execute(SQL_INSERT_ARTICLE, article_values)
+        self.conn.commit()
+        cur.close()
+
+    def insert_ranking(self, ranking_values):
+        """insert ranking
+        """
+        cur = self.conn.cursor()
+        cur.execute(SQL_INSERT_RANKING, ranking_values)
+        self.conn.commit()
+        cur.close()
+
+    def crawl_article(self, art_id, col_id, title, full_title, mag_name, url_first):
+        """crawl_article
+        """
+        self.logger.info('fetching article[%s] %s...', art_id, title)
+
+        if mag_name in ('TAIPEI', 'Discover Taipei'):
+            self.logger.info(
+                '      -> article[%s] in foreign language magazine (%s)', art_id, mag_name)
             return
 
-        text = ''
-        url_base = urljoin(NEWARTICLE, url_first)
+        if self.contain_article(art_id):
+            self.logger.info('      -> article[%s] contained and skip', art_id)
+            return
+
+        cont = ''
+        page_cnt = 0
+        url_base = urljoin(URL_NEWARTICLE, url_first)
         url_page = url_first
         while url_page is not None:
-            print('.', end='', flush=True)
-            # self.sleep()
+            self.sleep(sec=0)
+            page_cnt += 1
             url = urljoin(url_base, url_page)
             soup = BeautifulSoup(urlopen(url), PARSER)
             div_contents = soup.find_all('div', {'class': 'content'})
             if div_contents is None or len(div_contents) == 0:
-                print(' -> content broken')
+                self.logger.warning(
+                    '      -> article[%s] content broken', art_id)
                 return
-            text += div_contents[0].text
+            cont += div_contents[0].text
             bnext_btns = soup.find_all('a', {'class': 'bnext'})
-            url_page = None if len(
-                bnext_btns) == 0 else bnext_btns[0].get('href')
+            if len(bnext_btns) == 0:
+                url_page = None
+            else:
+                url_page = bnext_btns[0].get('href')
             soup.decompose()
-        self.sleep()
 
-        # write
-        published = '{0}-{1}-{2}'.format(
-            url_first[9:13], url_first[13:15], url_first[15:17])
-        self.writer.write_article(
-            [art_id, col_id, self.columns[col_id], title, full_title, mag_name, published,
-             url_first, text])
-        print('saved')
-        return
+        pub_date = '{0}-{1}-{2}'.format(url_first[9:13],
+                                        url_first[13:15], url_first[15:17])
+        article_values = [art_id, col_id, self.columns[col_id],
+                          mag_name, pub_date, title, full_title, cont]
+        self.insert_article(article_values)
+        self.logger.info(
+            '      -> article[%s] %s %d paged saved', art_id, title, page_cnt)
 
-    def fetch_month(self, year, month, col_id):
-        """fetch_mont
+    def crawl_month(self, year, month, col_id, page_size):
+        """crawl_month
         """
-        print('{0} [INFO] Processing Top100 of {1}-{2:02} at {3}'.format(
-            datetime_iso(), year, month, self.columns[col_id]), end='', flush=True)
-        page_size = 100
+        self.logger.info('fetching TOP 100 of %4d-%02d at %s',
+                         year, month, self.columns[col_id])
         start_date, end_date = month_range(year, month)
         req_body_json = '{{"Start":"{2}","End":"{3}","ColumnID":{0},"PageSize":{1},"PageIndex":1}}'
-        req_data = req_body_json.format(
-            col_id, page_size, start_date, end_date)
-        req = Request(url=NEWARTICLE, data=req_data.encode(encoding='utf_8'))
+        data = req_body_json.format(col_id, page_size, start_date, end_date)
+        req = Request(url=URL_NEWARTICLE, data=data.encode(encoding='utf_8'))
         req.add_header('Content-Type', 'application/json')
         with urlopen(req) as resp:
             data = json.loads(resp.read().decode('utf-8'))['d']
             if data['List'] is None:
-                print(' -> No List')
+                self.logger.info('      -> No List')
                 return
-            print(' -> {0} articles'.format(len(data['List'])))
+            self.logger.info('      -> %d articles', len(data['List']))
             for idx, art in enumerate(data['List']):
                 art_id = art['ArticleID']
                 full_title = art['FullTitle']
                 mag_name = art['MagName']
                 title = art['Title']
                 url = art['Url']
-                self.writer.write_ranking(
+                self.insert_ranking(
                     [year, month, col_id, idx + 1, self.columns[col_id],
                      art_id, title, full_title, mag_name, url])
-                self.fetch_article(art_id, col_id, title,
+                self.crawl_article(art_id, col_id, title,
                                    full_title, mag_name, url)
 
     def fetch_all(self):
         """fetch_all
         """
         start = (2013, 7)
-        end = (2016, 12)
+        end = (2017, 6)
+        page_size = 100
         for year in range(start[0], end[0] + 1):
             start_month = start[1] if year == start[0] else 1
             end_month = end[1] if year == end[0] else 13
             for month in range(start_month, end_month):
                 for col_id in self.columns:
-                    self.fetch_month(year, month, col_id)
+                    self.crawl_month(year, month, col_id, page_size)
 
     def calc_all(self):
         """calc_all
         """
         self.writer.select_articles()
-
-    def calc_one(self):
-        """calc_one
-        """
-
-    def update_published(self):
-        """update_published
-        """
-        self.writer.update_published()
 
 
 def print_usage():
@@ -275,17 +324,15 @@ if __name__ == '__main__':
         sys.exit(0)
     elif sys.argv[1] == 'fetch':
         WRITER = SqliteWriter()
-        MAG = MagCnyes(WRITER)
+        MAG = MagCnyesCrawler(WRITER)
         MAG.fetch_all()
         # MAG.fetch_month(2017, 4, 7)
-        # print(BOOK.test_book('0010723234'))
-        # print(BOOK.fetch_book('0010723234', '為了活下去：脫北女孩朴研美', '朴研美', 5))
-        # BOOK.fetch_book('0010723234', '為了活下去：脫北女孩朴研美', '朴研美')
     elif sys.argv[1] == 'calc':
         WRITER = SqliteWriter()
-        MAG = MagCnyes(WRITER)
+        MAG = MagCnyesCrawler(WRITER)
         MAG.calc_all()
-    elif sys.argv[1] == 'date':
+    elif sys.argv[1] == 'test':
         WRITER = SqliteWriter()
-        MAG = MagCnyes(WRITER)
-        MAG.update_published()
+        CRAWLER = MagCnyesCrawler(WRITER)
+        # CRAWLER.crawl_article()
+        CRAWLER.crawl_month(2016, 5, 3, 20)
