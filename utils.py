eb1af2ad37e42f928c8ef9425d7366046b5e3781
Tzeng Yuxio <tzengyuxio@gmail.com>
2017-06-24 14:50:38 +0800

    Refactor fetch_book to crawl_book

--- utils.py
+++ utils.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """utilities
 """
-
-
-import calendar
+from calendar import monthrange
 from datetime import datetime
+
+PARSER = 'html.parser'
 
 
 def datetime_iso():
@@ -28,15 +28,15 @@ def is_unihan_ext(char):
     CJK Unified Ideographs Extension D: '\U0002b740' <= char <= '\U0002b81f'
     CJK Unified Ideographs Extension E: '\U0002b820' <= char <= '\U0002ceaf'
     """
-    return ('\u3400' <= char <= '\u4dbf' or  # CJK Unified Ideographs Extension A
-            '\U00020000' <= char <= '\U0002a6df' or  # CJK Unified Ideographs Extension B
-            '\U0002a700' <= char <= '\U0002ceaf')  # CJK Unified Ideographs Extension C,D,E
+    return ('\u3400' <= char <= '\u4dbf' or          # CJK Extension A
+            '\U00020000' <= char <= '\U0002a6df' or  # CJK Extension B
+            '\U0002a700' <= char <= '\U0002ceaf')    # CJK Extension C,D,E
 
 
 def month_range(year, month):
     """month_range
     """
-    first, last = calendar.monthrange(year, month)
+    first, last = monthrange(year, month)
     first_date = '{0:04}-{1:02}-{2:02}'.format(year, month, first)
     last_date = '{0:04}-{1:02}-{2:02}'.format(year, month, last)
     return first_date, last_date
