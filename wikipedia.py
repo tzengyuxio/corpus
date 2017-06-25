3acef150185c2b6b702ab434757113c4ee838087
Tzeng Yuxio <tzengyuxio@gmail.com>
2017-06-26 00:52:07 +0800

    Refactor wikipedia.py

--- wikipedia.py
+++ wikipedia.py
@@ -6,21 +6,30 @@ import json
 import logging
 import sqlite3
 import sys
+from datetime import datetime
 from time import sleep
 from urllib.request import urlopen
-from utils import datetime_iso, is_unihan
+
 from bs4 import BeautifulSoup
 
-PARSER = 'html.parser'
+from utils import PARSER, datetime_iso, is_unihan
 
-WIKI_LIST = 'https://zh.wikipedia.org/zh-tw/Wikipedia:%E7%89%B9%E8%89%B2%E6%9D%A1%E7%9B%AE'
-WIKI_ARTICLE = 'https://zh.wikipedia.org/zh-tw/{0}'
-
-SQL_CREATE_ARTICLES = '''
-CREATE TABLE IF NOT EXISTS
-    articles (title TEXT, category TEXT, href TEXT, cont TEXT,
-    PRIMARY KEY(title))
+SQL_CREATE_TABLE_ARTICLES = '''
+CREATE TABLE IF NOT EXISTS articles (
+    title TEXT, open_date TEXT, category TEXT, url TEXT, article TEXT,
+    PRIMARY KEY(title, open_date))
 '''
+SQL_CONTAIN_ARTICLE = '''
+SELECT 1 FROM articles WHERE title=?
+'''
+SQL_INSERT_ARTICLE = '''
+INSERT OR IGNORE INTO articles (title, open_date, category, url, article) VALUES (?, ?, ?, ?, ?)
+'''
+
+URL_WIKI_LIST = 'https://zh.wikipedia.org/zh-tw/Wikipedia:%E7%89%B9%E8%89%B2%E6%9D%A1%E7%9B%AE'
+URL_WIKI_ARTICLE = 'https://zh.wikipedia.org/zh-tw/{0}'
+
+
 # num_char:   number of character in raw_text except space and new-line
 # num_hanzi:  number of hanzi in raw_text
 # num_unique: number of unique hanze in raw_text
@@ -29,14 +38,8 @@ CREATE TABLE IF NOT EXISTS
     corpus (src TEXT, idx TEXT, raw_text TEXT, stats TEXT, num_char INTEGER, num_hanzi INTEGER, num_unique INTEGER,
     PRIMARY KEY(src, idx))
 '''
-SQL_INSERT_ARTICLES = '''
-INSERT OR IGNORE INTO articles VALUES (?, ?, ?, ?)
-'''
 SQL_INSERT_CORPUS = '''
 INSERT OR IGNORE INTO corpus VALUES (?, ?, ?, ?, ?, ?, ?)
-'''
-SQL_EXISTS_ARTICLES = '''
-SELECT 1 FROM articles WHERE title=?
 '''
 SQL_SELECT_ARTICLES = '''
 SELECT title, cont FROM articles
@@ -50,33 +53,9 @@ class SqliteWriter():
     def __init__(self):
         self.conn = sqlite3.connect('wikipedia.db')
         cur = self.conn.cursor()
-        cur.execute(SQL_CREATE_ARTICLES)
+        cur.execute(SQL_CREATE_TABLE_ARTICLES)
         self.conn.commit()
         cur.close()
-
-        self.corpus = sqlite3.connect('corpus.db')
-        cur = self.corpus.cursor()
-        cur.execute(SQL_CREATE_CORPUS)
-        self.corpus.commit()
-        cur.close()
-
-    def write_article(self, title, href, cate, cont):
-        """write_article
-        """
-        cur = self.conn.cursor()
-        cur.execute(SQL_INSERT_ARTICLES, (title, cate, href, cont))
-        self.conn.commit()
-        cur.close()
-
-    def contains_article(self, title):
-        """contains_article
-        """
-        cur = self.conn.cursor()
-        cur.execute(SQL_EXISTS_ARTICLES, [title])
-        self.conn.commit()
-        result = True if cur.fetchone() is not None else False
-        cur.close()
-        return result
 
     def select_articles(self):
         """select_articles
@@ -100,94 +79,139 @@ class SqliteWriter():
             num_unique = len(char_freq_table)
             stats = json.dumps(
                 char_freq_table, ensure_ascii=False, sort_keys=True).encode('utf-8')
-            cur_ins = self.corpus.cursor()
-            cur_ins.execute(SQL_INSERT_CORPUS, (src, idx, raw_text,
-                                                stats, num_char, num_hanzi, num_unique))
+            # cur_ins = self.corpus.cursor()
+            # cur_ins.execute(SQL_INSERT_CORPUS, (src, idx, raw_text,
+            # stats, num_char, num_hanzi, num_unique))
             print('{0} [INFO] Calc articles[{1}] ... num(char/hanzi/unique) = {2}/{3}/{4}'.format(
                 datetime_iso(), row[0], num_char, num_hanzi, num_unique))
-            cur_ins.close()
+            # cur_ins.close()
         cur.close()
-        self.corpus.commit()
+        # self.corpus.commit()
 
 
-class Wikipedia():
+class WikipediaCrawler():
     """Crawler of Wikipedia
     """
 
     def __init__(self, writer):
         self.writer = writer
         self.urlopen_count = 0
-        logging.basicConfig(
-            format='%(asctime)s %(levelname)s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+        self.init_db()
+        self.init_logger()
+        self.logger.info('---- [WikipediaCrawler] ---------------------------')
+
+    def init_db(self):
+        """init db
+        """
+        self.conn = sqlite3.connect('source-wikipedia.db')
+        cur = self.conn.cursor()
+        cur.execute(SQL_CREATE_TABLE_ARTICLES)
+        self.conn.commit()
+        cur.close()
+
+    def init_logger(self):
+        """init logger
+        """
+        self.logger = logging.getLogger('magcnyes')
+        self.logger.setLevel(logging.DEBUG)
+        formatter = logging.Formatter(
+            '%(asctime)s %(levelname)-7s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+
+        ifh = logging.FileHandler('crawler-magcnyes.log', encoding='utf-8')
+        ifh.setFormatter(formatter)
+        ifh.setLevel(logging.INFO)
+        wfh = logging.FileHandler(
+            'crawler-magcnyes-warn.log', encoding='utf-8')
+        wfh.setFormatter(formatter)
+        wfh.setLevel(logging.WARN)
+        dsh = logging.StreamHandler()
+        dsh.setFormatter(formatter)
+        dsh.setLevel(logging.DEBUG)
+
+        self.logger.addHandler(ifh)
+        self.logger.addHandler(wfh)
+        self.logger.addHandler(dsh)
 
     def sleep(self):
         """sleep
         """
+        self.urlopen_count += 1
         sleep(1)
+
+    def contain_article(self, title):
+        """check if contains article
+        """
+        cur = self.conn.cursor()
+        cur.execute(SQL_CONTAIN_ARTICLE, [title])
+        result = True if cur.fetchone() is not None else False
+        cur.close()
+        return result
+
+    def insert_article(self, article_values):
+        """insert article
+        """
+        cur = self.conn.cursor()
+        cur.execute(SQL_INSERT_ARTICLE, article_values)
+        self.conn.commit()
+        cur.close()
 
     def fetch_article(self, idx, title, href, cate):
         """fetch_article
         """
-        print('{0} [INFO] {3:03} Fetching Article: [{1}]({2})'.format(
-            datetime_iso(), title, href, idx), end='', flush=True)
-        if self.writer.contains_article(title):
-            print(' -> contained and skip')
+        self.logger.info('%03d fetching article [%s](%s)...', idx, title, href)
+        if self.contain_article(title):
+            self.logger.info(
+                '          -> article [%s] contained and skip', title)
             return
-        print('.', end='', flush=True)
         self.sleep()
-        url = WIKI_ARTICLE.format(href[6:])
+        url = URL_WIKI_ARTICLE.format(href[6:])
         soup = BeautifulSoup(urlopen(url), PARSER)
 
         # remove siteSub
         soup.find(id='siteSub').extract()
-        print('.', end='', flush=True)
         # remove TOC
         if soup.find(id='toc' is not None):
             soup.find(id='toc').extract()
-        print('.', end='', flush=True)
         # remove div class=refbegin (reference)
         for elem in soup.find_all('div', {'class': 'refbegin'}):
             elem.extract()
-        print('.', end='', flush=True)
         # remove div class=reflist (reference)
         for elem in soup.find_all('div', {'class': 'reflist'}):
             elem.extract()
-        print('.', end='', flush=True)
         # remove all edit tag
         for elem in soup.find_all('span', {'class': 'mw-editsection'}):
             elem.extract()
-        print('.', end='', flush=True)
         # remove table infoBox
         for elem in soup.find_all('table', {'class': 'infobox'}):
             elem.extract()
-        print('.', end='', flush=True)
         # remove table navbox
         for elem in soup.find_all('table', {'class': 'navbox'}):
             elem.extract()
-        print('.', end='', flush=True)
         # remove table succession-box
         for elem in soup.find_all('table', {'class': 'succession-box'}):
             elem.extract()
-        print('.', end='', flush=True)
         # remove class=noprint
         for elem in soup.find_all(attrs={'class': 'noprint'}):
             elem.extract()
-        print('.', end='', flush=True)
 
         cont = soup.find(id='mw-content-text')
         if cont is None:
-            print(' -> ERROR: no content')
+            self.logger.error(
+                '          -> article [%s] has no content', title)
             return
 
         soup.decompose()
-        self.writer.write_article(title, href, cate, cont.text)
-        print('{0} char saved'.format(len(cont.text)))
+        the_date = datetime.now().strftime('%Y-%m-%d')
+        art_val = [title, the_date, cate, href, cont.text.strip()]
+        self.insert_article(art_val)
+        self.logger.info(
+            '          -> article [%s] with %d char saved', title, len(cont.text))
         return
 
     def fetch_all(self):
         """fetch_all
         """
-        soup = BeautifulSoup(urlopen(WIKI_LIST), PARSER)
+        soup = BeautifulSoup(urlopen(URL_WIKI_LIST), PARSER)
 
         # starting tag node of featured article links
         node = soup.find(id='content').find_all('table')[3].find_all('td')[0]
@@ -228,7 +252,7 @@ class Wikipedia():
             elif child.name == 'ul':
                 break
             else:
-                print('[Error] unexpected tag: <{0}>'.format(child.name))
+                self.logger.error('unexpected tag: <%s>', child.name)
                 break
 
         soup.decompose()
@@ -243,29 +267,25 @@ class Wikipedia():
         """
         self.writer.select_articles()
 
-    def calc_one(self):
-        """calc_one
-        """
-
 
 def print_usage():
     """Print Usage
     """
     print('usage: {0} command'.format(sys.argv[0]))
     print('')
-    print('    fetch   fetch ')
+    print('    all     fetch all')
 
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
         print_usage()
         sys.exit(0)
-    elif sys.argv[1] == 'fetch':
+    elif sys.argv[1] == 'all':
         WRITER = SqliteWriter()
-        WIKI = Wikipedia(WRITER)
+        WIKI = WikipediaCrawler(WRITER)
         WIKI.fetch_all()
         # print(WIKI.fetch_article('天津市耀華中學', '/wiki/%E5%A4%A9%E6%B4%A5%E5%B8%82%E8%80%80%E5%8D%8E%E4%B8%AD%E5%AD%A6'))
     elif sys.argv[1] == 'calc':
         WRITER = SqliteWriter()
-        WIKI = Wikipedia(WRITER)
+        WIKI = WikipediaCrawler(WRITER)
         WIKI.calc_all()
